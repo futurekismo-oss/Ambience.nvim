a51384fb2110a7f55e4a6aa4880edfafba55af14
FutureKismo <futurekismo@gmail.com>
2026-03-19 19:37:40 +0100

    Fixed track name bug

--- lua/ambience.lua
+++ lua/ambience.lua
@@ -88,14 +88,20 @@ function M.switch()
 end
 
 function M.now_playing()
-    if job_id == nil then return "" end
-    local result = vim.fn.system('echo \'{"command": ["get_property", "media-title"]}\' | socat - ' .. socketfile)
-    local ok, data = pcall(vim.fn.json_decode, result)
-    if ok and data and data.data then
-        local prefix = paused and "󰏤 " or "🎵 "
-        return prefix .. data.data
-    end
-    return ""
+	if job_id == nil then
+		return ""
+	end
+	local result = vim.fn.system('echo \'{"command": ["get_property", "media-title"]}\' | socat - ' .. socketfile)
+	local ok, data = pcall(vim.fn.json_decode, result)
+	if ok and data and data.data then
+		for _, track in ipairs(config.tracks) do
+			if track[2] == data.data then
+				local prefix = paused and "󰏤 " or "🎵 "
+				return prefix .. track[1]
+			end
+		end
+	end
+	return ""
 end
 
 function M.setup(opts)
